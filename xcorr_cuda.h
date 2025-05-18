c0893aefbb61825e582bb4637e9b046e47e40b7d
Aminxh <127418813+Aminxh@users.noreply.github.com>
2025-05-18 22:33:30 +0330

    Create xcorr_cuda.h
    header file for xcorr_cuda

---
+++ xcorr_cuda.h
@@ -0,0 +1,17 @@
+#ifndef XCORR_CUDA_H
+#define XCORR_CUDA_H
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cuComplex.h>
+#include "cufft.h"
+#include <iostream>
+#include <cmath>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+vector<vector<float>> runCAF(cufftComplex* primSig, cufftComplex* secondSig, int64_t lenPrimSig, int64_t lenSecondSig);
+
+#endif // XCORR_CUDA_H
